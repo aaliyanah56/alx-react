bbbcef1728b0d884f88b8e1787fa846599773304
aaliyanah56 <100027056+aaliyanah56@users.noreply.github.com>
2023-01-23 20:54:37 +0300

    webpack

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,12 +1,10 @@
-const path = require("path");
+const path = require('path')
 
 module.exports = {
-  mode: 'production',
-  entry: {
-    main: path.resolve(__dirname, './js/dashboard_main.js'),
-  },
+  entry: './js/dashboard_main.js',
   output: {
-    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
   },
+  mode: 'production',
 };
